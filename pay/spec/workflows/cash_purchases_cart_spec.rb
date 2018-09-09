624ebb6710df709b0d88be3361ce6220138f324f
takagotch <dyaccb@gmail.com>
2018-09-10 06:13:41 +0900

    raise  '' unless pre_purchase_valid? self.refund_payment = generate_refund_payment.payment update_tickets RefundChargeJob.perform_later() self.success = true

--- pay/spec/workflows/cash_purchases_cart_spec.rb
+++ pay/spec/workflows/cash_purchases_cart_spec.rb
@@ -3,51 +3,70 @@ require "rails_helper"
 describe CashPurchaseCart, :aggregate_failures do
   
   describe "successful cash purchase" do
-    let() {}
-    let() {}
-    let() {}
+    let(:performance) { create(:performance, event: create(:event)) }
+    let(:reference) { Payment.generate_reference }
+    let(:ticket_1) { create(
+      :ticket, status: "waiting",
+        price: Money.new(1500), performance: performance,
+        payment_reference: "reference") }
 
-    let() {}
-    let() {}
-    let() {}
-    let() {}
-    let() {}
-    let() {}
-    let() {}
-    let() {}
-    let() {}
+    let(:ticket_2) { create(
+      :ticket, status: "waiting",
+        price: Money.new(1500), performance: performance,
+        payment_reference: "reference") }
+    let(:ticket_3) { create(
+      :ticket, status: "unsold",
+        performance: performance,
+        payment_reference: "reference") }
+    let(:user) { create(:user) }
+    let(:discount_code) { nil }
+    let(:discount_code_string) { nil }
+    let(:workflow) { CashPurchasesCart.new(
+      user: user,
+      purchase_amount_cents: 3000,
+      expected_ticket_ids: "1 2",
+      payment_reference: "reference",
+      discount_code_string: discount_code_string) }
+    let(:attributes) { {user_id: user.id, price_cents: 3000,
+      reference: a_truthy_value, payment_method: "cash",
+      status: "succeeded", administrator_id: user.id,
+      discount_code_id: nil, discount:Money.zero} }
+    let(:attributes) { {user_id: user.id, price_cents: 3000,
+      reference: a_truthy_value, payment_method: "cash",
+      status: "succeeded", administrator_id: user.id,
+      discount_code_id: nil, discount: Money.zero} }
 
-    context "" do
-      before() do
-        allow().to receive().and_return()
-	[].each {}
+    context "woth an administrative user" do
+      before(:example) do
+        allow(user).to receive(:admin?).and_return(true)
+	[ticket_1, ticket_2].each { |t| t.place_in_cart_for(user) }
       end
 
-      it "" do
+      it "updates the ticket status" do
         workflow.run
-	expect().to be_purchased
-	expect().to be_purchased
-	expect().not_to be_purchased
-	expect().to be_truthy
-	expect().to match()
-	expect().to be_truthy
-	expect().to eq()
+	expect(Ticket.find(ticket_1.id)).to be_purchased
+	expect(Ticket.find(ticket_2.id)).to be_purchased
+	expect(Ticket.find(ticket_3.id)).not_to be_purchased
+	expect(workflow.success).to be_truthy
+	expect(workflow.payment_attributes).to match(attributes)
+	expect(workflow.success).to be_truthy
+	expect(workflow.payment.payment_line_items.size).to eq(2)
       end
     end
 
-    context "" do
+    context "with a regular user" do
     
-      before() do
-        allow().to receive().and_return()
+      before(:example) do
+        allow(user).to receive(:admin).and_return(false)
       end
 
-      it "" do
-        expect {}.to raise_error()
-	expect().to be_falsy
-	expect().to be_nil
-	expect().not_to be_purchased
-	expect().not_to be_purchased
-	expect().not_to be_purchased
+      it "fails" do
+        expect { workflow.run }.to raise_error(UnauthorizedPurchaseException)
+        expect(workflow.success?).to be_falsy
+        expect(workflow.payment).to be_nil
+        expect(Ticket.find(ticket_1.id)).not_to be_purchased
+        expect(Ticket.find(ticket_2.id)).not_to be_purchased
+        expect(Ticket.find(ticket_3.id)).not_to be_purchased
       end
 
     end
