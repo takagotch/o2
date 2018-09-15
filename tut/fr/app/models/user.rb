7742e0bce8363c5466340c3ee8e527feba8ee4fc
takagotch <dyaccb@gmail.com>
2018-09-15 09:38:08 +0900

    render 'shared/error_messages', object: f.object f.password_field :password, class: 'form-control' f.submit 'Update password', class: 'btn btn-primary'

--- tut/fr/app/models/user.rb
+++ tut/fr/app/models/user.rb
@@ -2,6 +2,12 @@ class User < ApplicationRecord
  #has_many :microposts
   has_many :microposts, dependent: :destroy
 
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
+
+  private
+
 end
 
 
